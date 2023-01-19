3306ec0a210687b81b1b4adccd914d2ade485bcc
Rednata <maknatosh@gmail.com>
2023-01-19 16:47:09 +0700

    feat: preview load Img

--- script/modules/controlModal.js
+++ script/modules/controlModal.js
@@ -1,11 +1,13 @@
 import { showModal } from './showModal.js';
 import { changeForm, submitForm } from './functionForm.js';
+import { previewImg } from './getImgFile.js';
 
 const onfeaturesButtonClick = () => {
   const featuresButton = document.querySelector('.features__button');
   featuresButton.addEventListener('click', async () => {
     await showModal();
     changeForm();
+    previewImg();
     submitForm();
   });
 
---
+++ script/modules/getImgFile.js
@@ -0,0 +1,33 @@
+
+
+const previewImg = () => {
+  const fileInput = document.querySelector('.form__input');
+  const errorMessage = document.querySelector('.size__message');
+  const formWrapperLabelImg = document.querySelector('.form__wrapper-label-img');
+
+  fileInput.addEventListener('change', () => {
+    errorMessage.innerHTML = '';
+
+    if (document.querySelector('.form__img')) {
+      document.querySelector('.form__img').remove();
+    }
+
+    if (fileInput.files.length > 0) {
+      const sizeImg = fileInput.files[0].size;
+
+      if (sizeImg < 1048576) {
+        formWrapperLabelImg.className = 'form__addImg';
+        const previewImg = document.createElement('img');
+        previewImg.classList.add('form__img');
+        formWrapperLabelImg.append(previewImg);
+        const src = URL.createObjectURL(fileInput.files[0]);
+        previewImg.src = src;
+      } else {
+        errorMessage.textContent = 'Изображение не должно превышать размер 1 Мб';
+        formWrapperLabelImg.classList.add('form__addImg_error');
+      }
+    }
+  });
+};
+
+export {previewImg};
